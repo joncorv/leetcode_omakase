781b022e802ec3a9c6d5f4c9e6b9b0902e3f5e95
Jonathan Corriveau <joncorv@gmail.com>
2025-11-14 15:36:56 -0800

    wip longest palindromic substring

--- 5.longest-palindromic-substring.rs
+++ 5.longest-palindromic-substring.rs
@@ -1,16 +1,46 @@
 // @leet start
 impl Solution {
     pub fn longest_palindrome(s: String) -> String {
-        // define values
-        let s_len = s.len();
-        println!("The length of s is: {s_len}");
-        let result: String = "".to_string();
-        println!("the result string has been instantiated to: {result}");
+        let mut longest_string_size: usize = 0;
+        let mut result_string: String = String::new();
 
-        let s: Vec<char> = s.chars().collect();
-        println!("s has been shadow copied to: {:?}", &s);
+        for (index, current_char) in s.chars().enumerate() {
+            let mut count: usize = 0;
+            loop {
+                count += 1;
+                let (mut left_index, mut right_index): (isize, isize) =
+                    (index - count, index + count);
+                let (mut left_char, mut right_char): (char, char);
 
-        return "testing".to_string();
+                if let Some(left) = &s.get(left_char) {
+                    left_char = left;
+                }
+
+                if let Some(right) = &s.get(index + 1) {
+                    right_char = right;
+                }
+
+                if left_char == right_char {
+                    let current_length_found = (count * 2) + 1;
+                    if current_length_found > longest_string_size {
+                        longest_string_size = current_length_found;
+                        let temp_result_string = &s[left_index..right_index];
+                        result_string = temp_result_string.to_string();
+                        count += 1
+                    }
+                } else if count == 1 && (left_char == current_char || right_char == current_char) {
+                    if longest_string_size > 1 {
+                        break;
+                    } else {
+                        longest_string_size == 2
+                    }
+                } else {
+                    break;
+                }
+            }
+        }
+
+        return result_string;
     }
 }
 // @leet end
