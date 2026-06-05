1aecf26f94af840d172ecf525d75f8327c4407d4
joncorv <joncorv@gmail.com>
2026-06-04 17:18:36 -0700

    57 insert intervals

---
+++ 57.insert-interval.rs
@@ -0,0 +1,17 @@
+// @leet start
+impl Solution {
+    pub fn insert(intervals: Vec<Vec<i32>>, new_interval: Vec<i32>) -> Vec<Vec<i32>> {
+        let dummy: Vec<Vec<i32>> = Vec::new();
+        let index_found: Vec<usize> = Vec::new();
+
+        // fn is_intersecting_interval(interval: &Vec<i32>, new_interval: &Vec<i32>) -> bool {
+        //     if
+        //
+        // }
+        //
+
+        dummy
+    }
+}
+// @leet end
+
