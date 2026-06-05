74a9fbc783e8a7ec2e2bc87f7de62ca20dc8fc17
joncorv <joncorv@gmail.com>
2026-06-04 21:02:48 -0700

    57 insert interval wip

--- 57.insert-interval.rs
+++ 57.insert-interval.rs
@@ -10,6 +10,32 @@ impl Solution {
         // }
         //
 
+        // let mut intervals = intervals;
+
+        let mut no_overlap: Vec<Vec<i32>> = Vec::new();
+        let mut has_overlap: Vec<Vec<i32>> = Vec::new();
+
+        for interval in intervals {
+            println!("interval: {interval:?}");
+
+            let start_inside = new_interval[0] <= interval[1] && new_interval[0] >= interval[0];
+            let end_inside = new_interval[1] <= interval[1] && new_interval[1] >= interval[0];
+
+            if start_inside || end_inside {
+                has_overlap.push(interval.clone());
+            } else {
+                no_overlap.push(interval.clone());
+            }
+        }
+
+        println!("no_overlap: {no_overlap:?}");
+        println!("has_overlap: {has_overlap:?}");
+
+        let max_val;
+        let min_val;
+
+        for interval in has_overlap {}
+
         dummy
     }
 }
