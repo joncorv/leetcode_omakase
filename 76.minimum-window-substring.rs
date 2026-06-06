e14746c1d4a0352464ebabdb7d0f560909f8c041
joncorv <joncorv@gmail.com>
2026-06-05 21:22:33 -0700

    76 minimum window substring wip

--- 76.minimum-window-substring.rs
+++ 76.minimum-window-substring.rs
@@ -1,9 +1,43 @@
 // @leet start
 impl Solution {
     pub fn min_window(s: String, t: String) -> String {
-        // start here. this is a really fun one !!!
+        // the rules say this won't happen, but i'm doing it anyway
+        if s.is_empty() || t.is_empty() {
+            return "".to_string();
+        }
+
+        let small;
+        let large;
+
+        if s.len() < t.len() {
+            small = &s;
+            large = &t;
+        } else {
+            small = &t;
+            large = &s;
+        }
+
+        let (mut left, mut right): (usize, usize) = (0, small.len());
+
+        while right <= large.len() {
+            let mut testing_group: Vec<char> = large.get(left..right).unwrap().chars().collect();
+
+            for ch in small {
+                let found_index = testing_group.find(ch);
+
+                if let Some(idx) = testing_group.find(ch) {
+                    testing_group.remove(idx);
+                } else {
+                    break;
+                }
+            }
+
+            println!("iteration: {:?}", large.get(left..right));
+            left += 1;
+            right += 1;
+        }
+
         return "dummy".to_string();
     }
 }
 // @leet end
-
