093a0d945f4ffcd44a142e8cab6a0a5b5764ddbf
joncorv <joncorv@gmail.com>
2026-06-13 15:59:39 -0700

    739 daily temperatures speed adjustment --- now accepted

--- 739.daily-temperatures.rs
+++ 739.daily-temperatures.rs
@@ -4,10 +4,22 @@ impl Solution {
         let mut result = temperatures.clone();
         let num_temps = temperatures.len();
 
-        // let previous_temp = -1000000000;
+        let mut previous_temp = -1000000000;
 
         'main: for (start, temp) in temperatures.iter().enumerate() {
             let current_temp = temp.clone();
+
+            if current_temp == previous_temp {
+                let prev_result = result[start - 1];
+
+                if prev_result == 0 {
+                    result[start] = 0;
+                } else {
+                    result[start] = result[start - 1] - 1;
+                }
+                continue;
+            }
+            previous_temp = current_temp.clone();
 
             for end in ((start + 1)..num_temps) {
                 if temperatures[end] > current_temp {
